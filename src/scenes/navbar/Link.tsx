e06e39e54b877cf6fe047412fbd27a8684af0d04
ChrisNieves <nievescs20@gmail.com>
2023-01-16 19:28:32 -0500

    moved assets folder into src folder, created navbar component and link components for the navbar

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "@/shared/types";
+
+type Props = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+  //Treat variable lowerCasePage as a SelectedPage
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-primary-500" : ""
+      } transitiion hover: text-primary-300 duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+export default Link;
