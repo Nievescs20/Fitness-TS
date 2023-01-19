c583dbeb111868e7a087842e4e39463f7b6b170f
ChrisNieves <nievescs20@gmail.com>
2023-01-19 04:04:10 -0500

    created Benefits component

---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import {
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Benefits = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      ></motion.div>
+    </section>
+  );
+};
+
+export default Benefits;
